fa7ced5f55ea8730d9d00e02d8d7b26c167b430a
AlexKomanov <52656401+AlexKomanov@users.noreply.github.com>
2023-03-15 22:15:53 +0200

    Create testObjectTypes.ts

---
+++ testObjectTypes.ts
@@ -0,0 +1,13 @@
+// Task - Adding types to the object
+
+let worker : {
+    officeId :  number,
+    isOpened: false,
+    contacts: {
+        phone: string,
+        email: string,
+        address: {
+            city: string
+        }
+    }
+}
