e9057e343b065440838677fb56a3ecec4b7a5fa0
AlexKomanov <52656401+AlexKomanov@users.noreply.github.com>
2023-03-15 23:27:58 +0200

    Adding tuples.ts

---
+++ tuples.ts
@@ -0,0 +1,8 @@
+// Tuples
+
+const skills: [number, string] = [5, "DevOps"];
+
+// Construction like in React hooks
+const [id, skillName] = skills;
+
+const anotherArray: [number, string, ...boolean[]] = [2, "Developer", true, false, true];
