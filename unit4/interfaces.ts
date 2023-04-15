0b72a492657deccf7dcced4ba6f713f83f639801
AlexKomanov <52656401+AlexKomanov@users.noreply.github.com>
2023-04-15 07:24:43 +0300

    Update interfaces.ts

--- unit4/interfaces.ts
+++ unit4/interfaces.ts
@@ -1,11 +1,20 @@
-interface UserInterface {
+interface UserInterface extends RoleID{
     name: string,
     age: number,
     skills: string[]
 }
 
+interface RoleID {
+    roleId: number
+}
+
 let user1 : UserInterface = {
     name: 'alex',
     age: 33,
-    skills: ['', '']
+    skills: ['', ''],
+    roleId: 123
+}
+
+interface UserDictionary {
+    [index: number]: UserInterface,
 }
